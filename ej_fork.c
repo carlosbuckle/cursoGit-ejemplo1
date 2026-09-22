2e30eb2a951327057615c265cca3420584f731ff
Carlos Buckle <carlos.buckle@gmail.com>
2026-09-22 20:13:49 +0000

    feat(ej_fork): versión inicial

---
+++ ej_fork.c
@@ -0,0 +1,16 @@
+/* ej_fork.c
+   Ejemplo de uso del system call fork
+   autor CB
+*/
+
+#include <stdio.h>
+main(){
+  if (fork()==0){
+      printf("soy el hijo %i \n", getpid());
+      sleep(30);
+  } else {
+      wait();
+      printf("soy el padre %i \n", getpid());
+  }
+  printf("FIN\n");
+}
